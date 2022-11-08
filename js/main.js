8be0b38e223a3f18513515c098b1f8aea77dd9e4
Ibrahim <ibrachar68@gmail.com>
2022-11-08 20:13:26 +0100

    background img

--- js/main.js
+++ js/main.js
@@ -6,34 +6,35 @@ let tastoAnnulla= document.getElementById("annulla");
     let nomeCognome=document.getElementById("inputNomeCognome").value;
     let chilometri=document.getElementById("inputKm").value;
     // let eta=document.getElementById("inputEta").value;
-    let eta=document.getElementById("sel-eta");
-    let minorenne=document.getElementById("minorenne");
-    let maggiorenne=document.getElementById("maggiorenne");
-    let over65=document.getElementById("over65");
+    let eta=document.getElementById("select-eta");
+    // let minorenne=document.getElementById("minorenne");
+    // let maggiorenne=document.getElementById("maggiorenne");
+    // let over65=document.getElementById("over65");
     let carrozza=Math.floor(Math.random() *10)+1;
     let codiceCP=Math.floor(Math.random() *100000)+10000;
     let prezzo= 0.21 * chilometri;
     console.log(prezzo);
     let prezzoTot;
     let rounded;
-    if (eta < 18) {
+    if (eta < 18 ) {
         prezzoTot= prezzo-((prezzo / 100) * 20);
         rounded = prezzoTot.toFixed(2);
         console.log(prezzoTot);
-        eta===minorenne;
+        
+      
         
     }
-    else if (eta > 65){
+    else if (eta > 64){
         prezzoTot= prezzo-((prezzo / 100) * 40);
         rounded = prezzoTot.toFixed(2);
         console.log(prezzoTot);
-        eta===over65;
+        
     } 
-    else if (eta > 18 < 64) {
+    else if (eta > 18 && eta < 64)  {
         prezzoTot= prezzo;
         rounded = prezzoTot.toFixed(2);
         console.log(prezzoTot);
-        eta===maggiorenne;
+        
     }
 
     
