209285d26477d134490dd23d7dbb2f2cc852cb14
Ibrahim <ibrachar68@gmail.com>
2022-11-08 16:59:11 +0100

    numero random carrozza e codice

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 let tasto= document.getElementById("genera");
+let tastoAnnulla= document.getElementById("annulla");
 
   tasto.addEventListener("click", 
    function(){
     let nomeCognome=document.getElementById("inputNomeCognome").value;
     let chilometri=document.getElementById("inputKm").value;
     let eta=document.getElementById("inputEta").value;
+    let carrozza=Math.floor(Math.random() *10)+1;
+    let codiceCP=Math.floor(Math.random() *100000)+10000;
     let prezzo= 0.21 * chilometri;
     console.log(prezzo);
     let prezzoTot;
@@ -28,8 +31,17 @@ let tasto= document.getElementById("genera");
     
     let blocco= document.querySelector(".generatore2.mostra");
     blocco.style.display="block";
+
     let nomeBiglietto=document.getElementById("nomeBiglietto").innerHTML=`${nomeCognome}`
     let prezzoBiglietto=document.getElementById("prezzoBiglietto").innerHTML=`${prezzoTot}\u20AC `
+    let postoCarrozza=document.querySelector(".valore-carrozza").innerHTML=`${carrozza}`;
+    let codice=document.querySelector(".valore-codice").innerHTML=`${codiceCP}`;
+  }
+ );
 
+ annulla.addEventListener("click", 
+   function(){
+     let none= document.querySelector(".generatore2.mostra")
+     none.style.display="none";
   }
  );
