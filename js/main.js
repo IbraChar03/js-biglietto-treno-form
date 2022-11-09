e43bc4ecff56e7f4f5014869a1ba246985fcfbcb
Ibrahim <ibrachar68@gmail.com>
2022-11-09 10:20:43 +0100

    select età

--- js/main.js
+++ js/main.js
@@ -5,18 +5,14 @@ let tastoAnnulla= document.getElementById("annulla");
    function(){
     let nomeCognome=document.getElementById("inputNomeCognome").value;
     let chilometri=document.getElementById("inputKm").value;
-    // let eta=document.getElementById("inputEta").value;
-    let eta=document.getElementById("select-eta");
-    // let minorenne=document.getElementById("minorenne");
-    // let maggiorenne=document.getElementById("maggiorenne");
-    // let over65=document.getElementById("over65");
+    let eta=document.getElementById("select-eta").value;
     let carrozza=Math.floor(Math.random() *10)+1;
     let codiceCP=Math.floor(Math.random() *100000)+10000;
     let prezzo= 0.21 * chilometri;
     console.log(prezzo);
     let prezzoTot;
     let rounded;
-    if (eta < 18 ) {
+    if (eta === "minorenne" ) {
         prezzoTot= prezzo-((prezzo / 100) * 20);
         rounded = prezzoTot.toFixed(2);
         console.log(prezzoTot);
@@ -24,13 +20,13 @@ let tastoAnnulla= document.getElementById("annulla");
       
         
     }
-    else if (eta > 64){
+    else if (eta === "over65"){
         prezzoTot= prezzo-((prezzo / 100) * 40);
         rounded = prezzoTot.toFixed(2);
         console.log(prezzoTot);
         
     } 
-    else if (eta > 18 && eta < 64)  {
+    else if (eta === "maggiorenne")  {
         prezzoTot= prezzo;
         rounded = prezzoTot.toFixed(2);
         console.log(prezzoTot);
@@ -42,7 +38,7 @@ let tastoAnnulla= document.getElementById("annulla");
     blocco.style.display="block";
 
     let nomeBiglietto=document.getElementById("nomeBiglietto").innerHTML=`${nomeCognome}`
-    let prezzoBiglietto=document.getElementById("prezzoBiglietto").innerHTML=`${prezzoTot}\u20AC `
+    let prezzoBiglietto=document.getElementById("prezzoBiglietto").innerHTML=`${rounded}\u20AC `
     let postoCarrozza=document.querySelector(".valore-carrozza").innerHTML=`${carrozza}`;
     let codice=document.querySelector(".valore-codice").innerHTML=`${codiceCP}`;
   }
